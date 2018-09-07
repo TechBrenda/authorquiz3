561d1cbcecd233104111d75da4197686c2af9324
TechBrenda <tech.brenda@gmail.com>
2018-09-07 15:35:43 -0500

    Restructure app

--- src/components/AuthorQuiz/AuthorQuiz.js
+++ src/components/AuthorQuiz/AuthorQuiz.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css';
+import '../AuthorQuiz/AuthorQuiz.css';
+import '../../style/bootstrap.min.css';
 
 function AuthorQuiz() {
 return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import AuthorQuiz from './AuthorQuiz';
+import './style/index.css';
+import AuthorQuiz from './components/AuthorQuiz/AuthorQuiz';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
