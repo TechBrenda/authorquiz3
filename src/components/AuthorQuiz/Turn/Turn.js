a615172de8b342bbe4e35695d836a36629a8120d
TechBrenda <tech.brenda@gmail.com>
2018-09-07 16:59:23 -0500

    React-Redux setup

---
+++ src/components/AuthorQuiz/Turn/Turn.js
@@ -0,0 +1,21 @@
+//import React from 'react';
+import { shuffle, sample } from 'underscore';
+import '../../../style/bootstrap.min.css';
+import './Turn.css';
+
+export function getTurnData(authors) {
+  const allBooks = authors.reduce(function (p,c,i) {
+    return p.concat(c.books);
+  }, []);
+  
+  const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+  const answer = sample(fourRandomBooks);
+  
+  return {
+    books: fourRandomBooks,
+    author: authors.find((author) =>
+      author.books.some((title) =>
+        title === answer))
+  };
+}
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
 import './style/index.css';
 import AuthorQuiz from './components/AuthorQuiz/AuthorQuiz';
 import registerServiceWorker from './registerServiceWorker';
+import authors from './authors.json';
+import { getTurnData } from './components/AuthorQuiz/Turn/Turn';
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+function reducer(
+  state = {
+    authors,
+    turnData: getTurnData(authors),
+    highlight: ''
+  }, action
+) {
+  switch (action.type) {
+    default: return state;
+  }
+}
+
+let store = Redux.createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ReactRedux.Provider store={store}>
+      <Route exact path="/" component={AuthorQuiz} />
+    </ReactRedux.Provider>
+  </BrowserRouter>
+  , document.getElementById('root'));
 registerServiceWorker();
